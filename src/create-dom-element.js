6f495cb62ff123be0250ef2fb5b7990202f2d414
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-23 13:36:35 +0100

    Create helper function for creating DOM elements
    Added a createDomElement function in create-dom-elements.js module.
    This is used in creating DOM elements and adding

---
+++ src/create-dom-element.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-use-before-define */
+
+function createDomElement(type, attributes) {
+  const newElement = document.createElement(`${type}`);
+  if (attributes == null) return newElement;
+  setAttributes(newElement, attributes);
+  return newElement;
+}
+
+function setAttributes(element, attributes) {
+  Object.entries(attributes).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+}
+
+export { createDomElement };
