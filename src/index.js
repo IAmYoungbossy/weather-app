a339d545e4170720d62b6f545b3a72b79a087137
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-23 20:14:32 +0100

    Add next7DaysForecast function for getting 7 days forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+/* eslint-disable no-use-before-define */
 import "./style.css";
 
-console.log("Check");
+const cityName = "lagos state";
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=1d849694bc0f4283fc1f008f1259542e`,
+  {
+    mode: "cors",
+  },
+)
+  .then((response) => response.json())
+  .then((response) => {
+    const { lat } = response.coord;
+    const { lon } = response.coord;
+    next7daysForecast(lat, lon);
+  })
+  .catch((error) => console.log(error));
+
+function next7daysForecast(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=7&appid=1d849694bc0f4283fc1f008f1259542e`,
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response));
+}
