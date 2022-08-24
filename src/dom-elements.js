17314c8626b80e898d317fcd09e09e51837ecc2c
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-24 14:54:35 +0100

    Position header and children alements in right positions

--- src/dom-elements.js
+++ src/dom-elements.js
@@ -2,11 +2,21 @@ import { createDomElement } from "./create-dom-element";
 
 function createHeader() {
 	const header = createDomElement("header");
-	const searchBar = createDomElement("input", { type: "search" });
+	const searchDiv = createDomElement("div", { class: "search-div" });
+	const searchBar = createDomElement("input", {
+		type: "search",
+		placeholder: "Enter City Name",
+	});
 	const searchButton = createDomElement("button", { type: "button" });
+	const headerTitleDiv = createDomElement("div", { class: "title-div" });
+	const headerTitle = createDomElement("h1");
 
-  searchButton.textContent = "Search";
-	header.append(searchBar, searchButton);
+	searchButton.textContent = "Search";
+	headerTitle.textContent = "CityWatch";
+
+	searchDiv.append(searchBar, searchButton);
+	headerTitleDiv.append(headerTitle);
+	header.append(headerTitleDiv, searchDiv);
 	document.body.append(header);
 }
 
