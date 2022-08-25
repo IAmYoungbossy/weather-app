721472eeafca0cfee9a57be1f57625cc348412a6
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-25 17:46:48 +0100

    Add minorDataReport function

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -40,6 +40,17 @@ function displayWeatherReport(response) {
 		feelsLikeTemp,
 	] = dataDisplay();
 
+	const [
+		wind,
+		seaLevel,
+		groundLevel,
+		timeZone,
+		date,
+		pressure,
+		humidity,
+		sunriseSunset,
+	] = minorDataReport();
+
 	const [regionName] = getCountryName(response);
 	weatherDesc.textContent = `${response.weather[0].description}`;
 	nameOfCity.textContent = `${regionName}`;
@@ -63,6 +74,14 @@ function displayWeatherReport(response) {
 		convertKelvinToCelsius(response.main.feels_like),
 		superScript()
 	);
+	wind.append(`Wind Speed: ${response.wind.speed}`);
+	seaLevel.append(`Sea Level: ${response.main.sea_level}`);
+	groundLevel.append(`Ground Level: ${response.main.grnd_level}`);
+	timeZone.append(`time Zone: ${response.timezone}`);
+	date.append(`Current Date: ${response.dt}`);
+	pressure.append(`Pressure: ${response.main.pressure}`);
+	humidity.append(`Humidity: ${response.main.humidity}`);
+	sunriseSunset.append(`Sunset/Sunrise: ${response.sys.sunrise}/${response.sys.sunset}`);
 
 	descIcon.src = `https://openweathermap.org/img/w/${response.weather[0].icon}.png`;
 }
@@ -76,6 +95,60 @@ function getCountryName(response) {
 
 function convertKelvinToCelsius(response) {
 	return parseInt(response) - 273;
+}
+
+function minorDataReport() {
+	const minorDataDiv = document.body.childNodes[2].childNodes[0].childNodes[3];
+	const windDiv = createDomElement("div", { class: "wind-div" });
+	const wind = createDomElement("p", { class: "wind" });
+	const seaLevelDiv = createDomElement("div", { class: "sea-level-div" });
+	const seaLevel = createDomElement("p", { class: "sea-level" });
+	const groundLevelDiv = createDomElement("div", { class: "ground-level-div" });
+	const groundLevel = createDomElement("p", { class: "ground-level" });
+	const timeZoneDiv = createDomElement("div", { class: "time-zone-div" });
+	const timeZone = createDomElement("p", { class: "time-zone" });
+	const dateDiv = createDomElement("div", {
+		class: "date-div",
+	});
+	const date = createDomElement("p", { class: "date" });
+	const pressureDiv = createDomElement("div", { class: "pressure-div" });
+	const pressure = createDomElement("p", { class: "pressure" });
+	const humidityDiv = createDomElement("div", { class: "humidity-div" });
+	const humidity = createDomElement("p", { class: "humidity" });
+	const sunriseSunsetDiv = createDomElement("p", {
+		class: "sunrise-sunset-div",
+	});
+	const sunriseSunset = createDomElement("p", { class: "sunrise-sunset" });
+
+	windDiv.append(wind);
+	seaLevelDiv.append(seaLevel);
+	groundLevelDiv.append(groundLevel);
+	timeZoneDiv.append(timeZone);
+	dateDiv.append(date);
+	pressureDiv.append(pressure);
+	humidityDiv.append(humidity);
+	sunriseSunsetDiv.append(sunriseSunset);
+	minorDataDiv.append(
+		windDiv,
+		seaLevelDiv,
+		groundLevelDiv,
+		timeZoneDiv,
+		dateDiv,
+		pressureDiv,
+		humidityDiv,
+		sunriseSunsetDiv
+	);
+
+	return [
+		wind,
+		seaLevel,
+		groundLevel,
+		timeZone,
+		date,
+		pressure,
+		humidity,
+		sunriseSunset,
+	];
 }
 
 function superScript() {
@@ -98,11 +171,12 @@ function dataDisplay() {
 	const feelsLikeTemp = createDomElement("p", { class: "feels-like-temp" });
 	const temperatureDiv = createDomElement("div", { class: "temperature-div" });
 	const temperature = createDomElement("p", { class: "temperature" });
+	const minorDataDiv = createDomElement("div", { class: "minor-data-div" });
 
 	tempRange.append(lowTemp, highTemp);
 	temperatureDiv.append(temperature, descIcon);
 	tempDiv.append(tempRange, temperatureDiv, feelsLikeTemp);
-	todayDataDiv.append(weatherDesc, nameOfCity, tempDiv);
+	todayDataDiv.append(weatherDesc, nameOfCity, tempDiv, minorDataDiv);
 
 	return [
 		weatherDesc,
