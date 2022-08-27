40b062ef7a1d4761de6d201a5b91bbb3eb0a4fc2
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-27 10:43:34 +0100

    Remove border outline from watchlist input element

--- src/city-watchlist.js
+++ src/city-watchlist.js
@@ -1,12 +1,20 @@
 import { createDomElement } from "./create-dom-element";
+import AddIcon from "./icons/addcity.png";
 
 function watchlist() {
 	const watchlistDiv = document.body.childNodes[2].childNodes[2],
 		watchlistHeader = createDomElement("h2"),
 		cityList = createDomElement("ul"),
-		addCityButton = createDomElement("li");
+		addCityButton = createDomElement("li", { class: "add-button" }),
+		watchlistInput = createDomElement("input", {
+			class: "watchlist-input",
+			type: "search",
+			placeholder: "Add City",
+		}),
+		MyAddIcon = new Image();
+	MyAddIcon.src = AddIcon;
 
-	addCityButton.textContent = "Add City";
+	addCityButton.append(MyAddIcon, watchlistInput);
 	watchlistHeader.textContent = "Watchlist";
 	cityList.append(addCityButton);
 	watchlistDiv.append(watchlistHeader, cityList);
